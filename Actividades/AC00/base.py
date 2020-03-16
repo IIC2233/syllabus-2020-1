183bc36f6f524cfd2d02248b73537db274c7f0e3
entamburini <entamburini@uc.cl>
2020-03-16 16:21:17 -0300

    Revert "Add base Actividades folder"
    This reverts commit 25d539101bef0824ca4d10ac3ad4790a0acd094c.

--- Actividades/AC00/base.py
+++ Actividades/AC00/base.py
@@ -1,33 +1,17 @@
 # Seccion para importar liberías
 
 class Cuadrado(object):
-    def __init__(self, arg):
-        self.lado = arg
-        self.angulo = 90
-
-    def area(self):
-        return self.lado * self.lado
-
-    def perimetro(self):
-        return 4 * self.lado
+	def __init__(self, arg):
+		# Completar método y borrar pass
+		pass
+	# Agregar métodos
 
 class Triangulo(object):
-    """
-    Se supone un triángulo equilátero de lado = arg
-    """
-    def __init__(self, arg):
-        self.lado = arg
-        self.base = self.lado
-        self.altura = self.lado * (3**(1/2)) / 2
-        self.angulo = 60
-
-    def area(self):
-        return self.base * self.altura / 2
-
-    def perimetro(self):
-        return 3 * self.lado
-
+	def __init__(self, arg):
+		# Completar método y borrar pass
+		pass		
+	# Agregar métodos
+		
+		
 if __name__ == '__main__':
-    # Crear instancias aquí
-    cuadrado = Cuadrado(2)
-    triangulo = Triangulo(2)
+	# Crear instancias aquí
