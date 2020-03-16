25d539101bef0824ca4d10ac3ad4790a0acd094c
Matias Duhalde <matias.duhalde@uc.cl>
2020-03-16 15:54:07 -0300

    Add base Actividades folder

--- Actividades/AC00/base.py
+++ Actividades/AC00/base.py
@@ -1,17 +1,33 @@
 # Seccion para importar liberías
 
 class Cuadrado(object):
-	def __init__(self, arg):
-		# Completar método y borrar pass
-		pass
-	# Agregar métodos
+    def __init__(self, arg):
+        self.lado = arg
+        self.angulo = 90
+
+    def area(self):
+        return self.lado * self.lado
+
+    def perimetro(self):
+        return 4 * self.lado
 
 class Triangulo(object):
-	def __init__(self, arg):
-		# Completar método y borrar pass
-		pass		
-	# Agregar métodos
-		
-		
+    """
+    Se supone un triángulo equilátero de lado = arg
+    """
+    def __init__(self, arg):
+        self.lado = arg
+        self.base = self.lado
+        self.altura = self.lado * (3**(1/2)) / 2
+        self.angulo = 60
+
+    def area(self):
+        return self.base * self.altura / 2
+
+    def perimetro(self):
+        return 3 * self.lado
+
 if __name__ == '__main__':
-	# Crear instancias aquí
+    # Crear instancias aquí
+    cuadrado = Cuadrado(2)
+    triangulo = Triangulo(2)
---
+++ Actividades/README.md
@@ -0,0 +1,12 @@
+# Actividades
+
+Cada actividad **debe estar contenida en una carpeta llamada `AFXX` o `ASXX` donde `XX` es el número de la actividad.** El contenido de este directorio debería verse más o menos así, una vez avanzado el semestre:
+
+* ...
+* `AF06`
+* `AF07`
+* `AS01`
+* `AS02`
+* ...
+
+En caso de que durante una actividad hayas tomado cualquier decisión de diseño que pueda ser compleja o que creas que debes justificar o completar, puedes explicarla en un archivo llamado `README.md` (puedes usar [Markdown](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)) dentro de la carpeta de la AC correspondiente con información útil para el ayudante que corrija tu código.
